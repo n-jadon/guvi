c70a1c9d872a1610183a9302d5a3556da5c4dab4
n-jadon <35300781+n-jadon@users.noreply.github.com>
2018-01-14 05:51:36 +0530

    Largest Among 3

---
+++ fifth.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{ 
+    int a,b,c;
+    int largest;
+	scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c)        
+        largest=a;
+    else if(b>a && b>c)       
+        largest=b;
+    else
+        largest=c;
+ 
+    printf("%d",largest);
+ 
+    return 0;
+}
