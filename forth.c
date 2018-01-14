3d6ec8d7ae5854a88d7d0414ae150accd3b28ffb
n-jadon <35300781+n-jadon@users.noreply.github.com>
2018-01-14 05:39:21 +0530

    Alphabet or not

---
+++ forth.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("Alphabet",c);
+    else
+        printf("No",c);
+
+    return 0;
+}
